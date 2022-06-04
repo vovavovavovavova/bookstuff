0cf672b1001d502b8a040fe8a8f3731e149c68df
vova <vovchik4@bigmir.net>
2022-06-04 23:40:40 +0300

    disable debug

--- bookstuff/bookstuff/settings.py
+++ bookstuff/bookstuff/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a5dmfeae*+q$ik*lte#-&tc^yv&=nzgt809%^vg+*wkju)7efd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
